2a76106f30b380beeef1e33562be4ada6ebe700b
isabella garcia <isabellagarciasx@gmail.com>
2025-10-10 13:26:31 -0300

    modifiquei algumas coisa dos links

--- banco de dados/dml_devconnect.sql
+++ banco de dados/dml_devconnect.sql
@@ -1,33 +1,33 @@
 --Dml
-USE db_devconnect
+USE db_devconnect_M;
 
-INSERT INTO tb_devconnect(nome_devconnect,foto_logo_url)
-VALUES
-('devconnect','foto');
-SELECT * FROM tb_devconnect;
 
-INSERT INTO tb_usuario(nome_usuario,nome_completo,email,senha,foto_perfilb,id_devconnect)
-VALUES
-('isabella','isabella garcia','isa_bella@','1234','foto',1);
+INSERT INTO tb_usuario(nome_completo,nome_usuario,email,senha,foto_perfil_url)
+VALUES('edu','e.du_','edu@gmail.com','1234','www.com.DevConnect/bella');
+
 SELECT * FROM tb_usuario;
 
 INSERT INTO tb_publicacao(descricao,imagem_url,data_publicacao ,id_usuario)
 VALUES
-('oi seu sou a bella','foto','2025/03/06'1);
+('oi seu sou a bella','foto','2025/03/06',1);
+
 SELECT * FROM tb_publicacao;
 
-INSERT INTO tb_seguidor(id_usuario,id_usuario)
-VALUES
-(1,1);
+INSERT INTO tb_seguidor(id_seguindo, id_usuario_seguidor)
+VALUES(1,3);
+
 SELECT * FROM tb_seguidor;
 
 INSERT INTO tb_comentario(texto,data_comentario,id_publicacao, id_usuario)
 VALUES
-('dsdfsdgg','2025/03/18',1,1);
+('dsdfsdgg','2025-03-18',1,1);
+
 SELECT * FROM tb_comentario;
 
 
-INSERT INTO Tb_curtidas(id_publicacao,id_usuario)
+INSERT INTO Tb_curtida(id_publicacao,id_usuario)
 VALUES
-(1,1);
-SELECT * FROM Tb_curtidas;
+(1,3);
+
+
+SELECT * FROM Tb_curtida;
--- banco de dados/dql_devconnect.sql
+++ banco de dados/dql_devconnect.sql
@@ -1,5 +1,5 @@
 --DQl
-USE db_devconnect
+USE db_devconnect_M;
 
 SELECT nome_usuario FROM tb_usuario;
 
@@ -9,3 +9,31 @@ SELECT COUNT (imagem_url) AS imagens FROM tb_publicacao
 SELECT MAX (data_comentario) AS datas  FROM tb_comentario
 
 SELECT MIN (data_comentario) AS datas  FROM tb_comentario
+
+
+--Exiba todos os nomes dos seguidores (de quem esta seguindo e de quem será seguido)
+
+
+
+--Exiba quantos seguidores possui um respectivo usuário
+SELECT 
+U.nome_usuario
+FROM tb_seguidor SE
+INNER JOIN tb_usuario U ON SE.id_seguindo = U.id;
+
+--Exiba todas as publicações contendo a descrição, o caminho da imagem, o nome de usuário
+SELECT  
+PU.descricao,
+PU.imagem_url,
+US.nome_usuario
+FROM tb_publicacao PU
+INNER JOIN tb_usuario US ON PU.id = US.id;
+
+--Exiba todos os comentários com o nome e o texto de uma respectiva publicação
+
+SELECT
+C.texto,
+FROM tb_comentario C
+INNER JOIN tb_publicacao C ON C.id = PU.id;
+--Exiba a quantidade de curtidas de uma respectiva publicação.
+
